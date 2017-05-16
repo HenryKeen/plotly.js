d1e86959abcc6ae03d4224d5e6811d7b06700ab7
henrykeen <henry_keen@hotmail.com>
2017-05-16 16:09:56 +0100

    Add comment to `resolve` call

--- src/components/images/draw.js
+++ src/components/images/draw.js
@@ -99,6 +99,10 @@ module.exports = function draw(gd) {
                 var dataURL = canvas.toDataURL('image/png');
 
                 thisImage.attr('xlink:href', dataURL);
+                
+                // resolve promise in onload handler instead of on 'load' to support IE11
+                // see https://github.com/plotly/plotly.js/issues/1685 
+                // for more details
                 resolve();
             };
 
